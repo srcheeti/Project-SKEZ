7f83ebc0f2bafb7eee7d22005860de30ba41e194
Suhas <suhasraocheeti@gmail.com>
2024-01-27 16:09:47 -0500

    Added react packages and have, Have added format to Map.js

--- skez-ui/src/pages/Map.js
+++ skez-ui/src/pages/Map.js
@@ -1,1 +1,46 @@
 // Where the map will be located
+import React, { useState } from "react";
+import "./App.css"
+
+import {
+    ComposableMap,
+    Geographies,
+    Geography,
+    Marker,
+    Annotation,
+    ZoomableGroup
+} from "react-simple-maps";
+
+import ReactTooltip from "react-tooltip"
+
+const geoURL =
+    "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+
+
+function Map(){
+    return(
+        <div className="Map" style={{
+            width: "100%",
+            height: "100%",
+            display:"flex",
+            flexDirection:"column",
+            justifyContent: "center",
+            alignItems: "center"
+        }}>
+
+            <h1>Avalability Map</h1>
+            <div style={{ width: "1400px", borderStyle: "double"}}>
+                <ComposableMap data-tip="">
+                    <Geographies geography={geoURL}>
+                        {({geographies}) =>
+                            geographies.map((geo) =>(
+                                <Geography key={geo.rsmKey} geography={geo} />
+                            ))
+                        }
+                    </Geographies>
+                </ComposableMap>
+            </div>
+        </div>
+    );
+}
+export default Map;
