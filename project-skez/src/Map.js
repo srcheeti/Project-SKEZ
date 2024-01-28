4fac8b7f9707a762435459ed814d61f4ee57301f
Suhas <suhasraocheeti@gmail.com>
2024-01-27 21:46:57 -0500

    Fixed Pathing

--- project-skez/src/Map.js
+++ project-skez/src/Map.js
@@ -1,12 +1,12 @@
 //Availibility Map
 import map from './assets/map.png';
-import styles from './pages/Map.css';
+import styles from './Map.css';
 
 function Map() {
   return (
     <div className={styles['map-container']}>
       <h1 style={{ textAlign: "center" }}>Availability Map</h1>
-        <img src={map} className="image" alt="logo" />
+      <img src={map} className="image" alt="logo" />
     </div>
   );
 }
