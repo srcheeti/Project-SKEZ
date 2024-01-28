3b56c4967b99f82b5755038b5a5ed4fdcb5155ba
Suhas <suhasraocheeti@gmail.com>
2024-01-28 10:58:46 -0500

    Map changes

--- project-skez/src/components/map/map.jsx
+++ project-skez/src/components/map/map.jsx
@@ -1,7 +1,9 @@
 //Availibility Map
 import React, { useState } from "react";
-import map from './assets/map.png';
-import styles from './Map.css';
+import map from '../../assets/map.png';
+import styles from './map.css';
+import { Button } from 'react-native';
+
 
 const theme = {
     blue: {
@@ -14,44 +16,8 @@ const theme = {
     },
 };
 
-const Button = styled.button
-background - color: ${ (props) => theme[props.theme].default };
-color: white;
-padding: 5px 15px;
-border - radius: 5px;
-outline: 0;
-border: 0;
-text - transform: uppercase;
-margin: 10px 0px;
-cursor: pointer;
-box - shadow: 0px 2px 2px lightgray;
-transition: ease background - color 250ms;
-  &:hover {
-    background - color: ${ (props) => theme[props.theme].hover };
-}
-  &:disabled {
-    cursor: default ;
-    opacity: 0.7;
-}
-;
 
-Button.defaultProps = {
-    theme: "blue",
-};
 
-function clickMe() {
-    alert("You clicked me!");
-}
-
-const ButtonToggle = styled(Button)
-opacity: 0.7;
-  ${
-    ({ active }) =>
-        active &&
-
-        opacity: 1;
-}
-;
 
 
 
@@ -61,7 +27,10 @@ function Map() {
             <h1 style={{ textAlign: "Left" }}>Availability Map</h1>
             <img src={map} className="image" alt="logo" />
             <a href="https://react.school/" target="_blank">
-                <Button>Link</Button>
+                <Button
+                    title="Press me"
+                    onPress={() => Alert.alert('Simple Button pressed')}
+                />
             </a>
         </div>
     );
