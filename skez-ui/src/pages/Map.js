a33f16a1e1778ce68b34b7374a77fd2bc10012cd
Suhas <suhasraocheeti@gmail.com>
2024-01-27 19:55:43 -0500

    Map changes

--- skez-ui/src/pages/Map.js
+++ skez-ui/src/pages/Map.js
@@ -1,24 +1,13 @@
 //Availibility Map
 import map from './assets/map.png';
-import './App.css';
+import styles from './pages/Map.css';
 
 function Map() {
   return (
-    <div className="Map" style={{
-      width: "100%",
-      height: "100%",
-      display:"flex",
-      flexDirection:"column",
-      justifyContent: "center",
-      alignItems: "center"
-  }}>
-
-      <h1>Avalability Map</h1>
-      <div style={{ width: "1400px", borderStyle: "double"}}>
+    <div className={styles['map-container']}>
+      <h1 style={{ textAlign: "center" }}>Availability Map</h1>
         <img src={map} className="image" alt="logo" />
-      </div>
-  </div>
+    </div>
   );
 }
-
 export default Map;
