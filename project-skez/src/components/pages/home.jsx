551417e1fa01548f839ffefb4e2d4c5dd2f1dc43
Zooomulus <andelisun@gmail.com>
2024-01-28 11:43:54 -0500

    last changes

--- project-skez/src/components/pages/home.jsx
+++ project-skez/src/components/pages/home.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import './home.css'
+import styles from './home.module.css' // Make sure the file is named 'home.module.css'
+import map from '../../assets/map.png'
 
 function Home() {
     return (
-        <div>
-            Home
+        <div className={styles['map-container']}>
+            <h1 style={{ textAlign: "Left" }}>Availability Map</h1>
+            <img src={map} className="image" alt="logo" />
         </div>
-    )
+    );
 }
 
 export default Home;
-
--- project-skez/src/main.jsx
+++ project-skez/src/main.jsx
@@ -8,30 +8,30 @@ import Home from './components/pages/home.jsx'
 //z
 
 import {
-    createBrowserRouter,
-    RouterProvider,
-  } from "react-router-dom";
-  
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App />,
-    },
-    {
-        path: "/helloWorld",
-        element: <h1>Hello World</h1>
-        
-    }  ,
-    {
-        path: "/hoasdfasdfasdfasfdsme",
-        element: <Home />
-    }
-  ]);
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/helloWorld",
+    element: <h1>Availibility Map</h1>
+
+  },
+  {
+    path: "/hoasdfasdfasdfasfdsme",
+    element: <Home />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        {/* <App /> */}
-      <RouterProvider router={router} />
+  <React.StrictMode>
+    {/* <App /> */}
+    <RouterProvider router={router} />
 
-    </React.StrictMode>,
+  </React.StrictMode>,
 )
